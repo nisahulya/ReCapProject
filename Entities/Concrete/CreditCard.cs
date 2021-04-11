1c06753174ddd5afdda79b5fa0f33e87b372ed4f
nisahulya <nisahulya94@gmail.com>
2021-04-11 23:57:18 +0300

    CreditCard

---
+++ Entities/Concrete/CreditCard.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class CreditCard : IEntity
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string OwnerName { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/CreditCardsController.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditCardsController : ControllerBase
+    {
+        ICreditCardService _creditCardService;
+
+        public CreditCardsController(ICreditCardService creditCardService)
+        {
+            _creditCardService = creditCardService;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(CreditCard card)
+        {
+            var result = _creditCardService.Add(card);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
